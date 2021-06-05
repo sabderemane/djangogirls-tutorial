9abf6697db952c1abe5b431b1471423461a3f245
Sarah Abderemane <sarahabderemane@gmail.com>
2021-06-05 21:03:50 +0200

    Review templates and add css

--- app/settings.py
+++ app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l+e4q2&q1pgl^rawe9!--d%54j2w)sk7qj%_1!9z$*^pgtouoo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
